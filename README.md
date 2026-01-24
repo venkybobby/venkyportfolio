0b234547e0cc205303e8325c36c027fe61e970ff
Venky Ramanathan <venkydec.20@gmail.com>
2026-01-24 17:05:21 -0600

    Update repository references to venkyportfolio

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 This simple portfolio template is designed to showcase your past projects, career history, skill sets, and more.
 
-View the [Live Site](https://venkybobby.github.io/venky-portfolio-template/).
+View the [Live Site](https://venkybobby.github.io/venkyportfolio/).
 
 **This template is free to use, and no attribution is required.** You can fork or download this repository to customize it for your own use. Please don't forget to leave a ⭐ if you like this portfolio!
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
         <a href="https://github.com/venkybobby" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/venky0411" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/venkybobby/venky-portfolio-template" target="_blank" rel="noreferrer">Venky Ramanathan</a> with 💜</p>
+      <p>A portfolio designed & built by <a href="https://github.com/venkybobby/venkyportfolio" target="_blank" rel="noreferrer">Venky Ramanathan</a> with 💜</p>
     </footer>
   );
 }
